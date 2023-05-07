ab21df3057879bf623442286ce1031e54febab34
Mariano Eiro <marianoeiro@hotmail.com>
2023-05-07 11:19:36 -0300

    fix

---
+++ src/models/product.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const productCollection = 'products';
+
+const productSchema = new mongoose.Schema({
+    
+
+})
+
+export const productModel = mongoose.model(productCollection, productSchema);ñ
