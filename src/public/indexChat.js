6d633ff765f78b0c45f24c60474071a9345aa59f
Mariano Eiro <marianoeiro@hotmail.com>
2023-05-10 23:16:21 -0300

    fix

--- src/public/indexChat.js
+++ src/public/indexChat.js
@@ -4,6 +4,7 @@ const body = document.getElementById('body');
 const text_chat = document.getElementById('text_chat');
 const msg = document.getElementById('msg');
 const child = document.querySelector('#child')
+const nodo = document.getElementById('nodo');
 
 text_chat.addEventListener('keyup', (e)=>{
     if(e.key === 'Enter'){
@@ -14,14 +15,14 @@ text_chat.addEventListener('keyup', (e)=>{
 
 socket.on('chat', (data)=>{
     body.scrollTop = body.scrollHeight;
-    const nodo = document.getElementById('nodo');
     
     let child = '';
 
     data.forEach(msg =>{
+        child +=        
         `<div class="d-flex flex-row justify-content-end mb-4">
-                <div class="p-3 me-3 shadow-sm" style="border-radius: 15px; background-color: #d1f5e8;">
-                    <p id="msg_ext" class="small mb-0">${msg.message}</p>
+                <div class="p-3 me-3 shadow-sm">
+                    <p id="msg_chat" class="small mb-0">${msg.message}</p>
                 </div>
         </div>`
     })
